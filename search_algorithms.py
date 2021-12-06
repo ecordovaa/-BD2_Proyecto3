8272ca0700ff75ddc8d7404f5377818ff4a29839
ecordovaa <efcordovaa@gmail.com>
2021-12-05 23:16:31 -0500

    backend v2

--- search_algorithms.py
+++ search_algorithms.py
@@ -12,20 +12,24 @@ def knn_search(size, img_enc, k=8):
     for row_id, row in df.iterrows():
         if row_id == size:
             break
-        heappush(h, (float(local_face_distance(np.array(img_enc), np.array(list(row)[1:]))), row_id))
-    return [heappop(h)[1] for _ in range(k)]
+        dist = local_face_distance(np.array(img_enc), np.array(list(row)[1:]))
+        if row_id < k:
+            heappush(h, (float(-dist), row_id))
+        else:
+            heappushpop(h, (float(-dist), row_id))
+    knn = [heappop(h)[1] for _ in range(k)]
+    knn.reverse()
+    return knn
 
 def range_search(idx, img_enc, radio):
     reshaped_enc = np.array(img_enc).reshape(1,-1)
     left_bound = reshaped_enc - radio
     right_bound = reshaped_enc + radio
     bbox = np.concatenate((left_bound, right_bound), axis=1)
-    # Candidatos: Puntos que pertenecen a una region que intereseca el rango (1er filtro).
     candidates = list(idx.intersection(tuple(bbox[0]), objects=True))
     distances = []
     for c in candidates:
         distances.append((c.id, local_face_distance(list(c.bbox)[:df.shape[1]-1], reshaped_enc)))
-    # Puntos en el rango: candidatos que se encuentran en el rango (2do filtro).
     in_range_points = [(id, dist) for (id,dist) in distances if dist <= radio]
     in_range_points.sort(key=lambda x:x[1])
     return [point[0] for point in in_range_points]
